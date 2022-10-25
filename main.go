78c56b76d23193324a5c6290e4b1712e9657ec60
Gede Rico Wijaya <gricowijaya@gmail.com>
2022-10-25 16:48:55 +0800

    update

--- main.go
+++ main.go
@@ -6,16 +6,6 @@ import(
     "fmt"
 )
 
-// create the person struct 
-type person struct {
-  // first name of the person
-  first string 
-  // last name of the person
-  last string
-  // age of the person
-  age int
-}
-
 func main() {
     // example of person struct, we use firstPerson variable to create the person
     firstPerson := person {
---
+++ person.go
@@ -0,0 +1,12 @@
+package personstruct
+
+// create the person struct 
+type person struct {
+  // first name of the person
+  first string 
+  // last name of the person
+  last string
+  // age of the person
+  age int
+}
+
